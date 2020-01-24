5e1d9e752baf48c5dfd4eddb602c95e7b86db326
Jess Rezac <jess.rezac@gmail.com>
2020-01-24 17:00:55 -0600

    feat: Calculate glasses sold and profits in Day#play_game

--- lemonade-stand-api/app/models/day.rb
+++ lemonade-stand-api/app/models/day.rb
@@ -2,68 +2,56 @@ class Day < ApplicationRecord
     belongs_to :game
     enum weather: [ :thunderstorms, :cloudy, :sunny, :hot_and_sunny ]
 
-    # after_create :play_game
+    after_save :play_game
 
-    
+    private
 
     def play_game
 
         # customer volume is determined by r * (n + (n * v)), then that amount is rounded
 
-        # set n
+        # set charge_multiplier (n)
         if self.charge_per_glass <= 10 
             # if charge < 10 then 1200 N1 = (10 - profits) / 10 * .8 * 30 + 30
-            n = (10 - self.charge_per_glass) / 10 * 0.8 * 30 + 30
+            charge_multiplier = (10 - self.charge_per_glass) / 10 * 0.8 * 30 + 30
         else
             # else n1 = (10 ^ 2) * 30 / profits ^ 2)
-            n = (10**2) * 30 / self.charge_per_glass**2
+            charge_multiplier = (10**2) * 30 / self.charge_per_glass**2
         end
 
-        # set v
-        w = self.signs_made * 0.5
-        v = 1 - Math.log(w)   # v = 1 - (exp(w) * 1)  // EXP is inverse natural log of the argument
+        # set signs multiplier (v)
+        w = -self.signs_made * 0.5
+        signs_multiplier = 1 - Math.exp(w)   # v = 1 - (exp(w) * 1)  // EXP is inverse natural log of the argument
 
-        # set chance of rain and r
+        # set random event multiplier (r)
         case self.weather
         when "thunderstorms"
             # J = 30 + INT (RND(1) * 5) * 10 // that's the chance of rain
             # if it is rainy, R1 becomes 1 - J / 100
 
             chance_of_rain = 30 + (Math.random(1) * 5).floor * 10
-            r = 1 - chance_of_rain/100
+            random_event_multiplier = 1 - chance_of_rain/100
         when "hot and sunny"
-            r = 2
+            random_event_multiplier = 2
         else
-            r = 1
+            random_event_multiplier = 1
         end
 
-        customers = (r * (n + (n * v))).floor
+        customers = (random_event_multiplier * (charge_multiplier + (charge_multiplier * signs_multiplier))).floor
 
-        if customers > self.glasses_made
+        if customers >= self.glasses_made
             self.glasses_sold = self.glasses_made
         else
             self.glasses_sold = customers
         end
-        # n2 = glasses sold
-        # m = n2 * price * .01
-        # e = signs * cost of signs + lemonade * cost of lemonade
-        # profits = m - e
-        # assets = assets + profits
 
+        expenses = (self.cost_of_lemonade * self.glasses_made) + (self.cost_of_signs * self.signs_made)
+        income = self.glasses_sold * self.charge_per_glass
+        self.profits = ((income - expenses) * 0.01).round(2)
 
+        self.game.current_assets += self.profits
 
-        # n2 = R1 * (N1 + (N1 * V))
-        # if n2 < = lemonade made, then m = N2 * P(i) * .01
-        # else n2 = L(I)
-        # then calc profits
-
-        # R1 is 1 at the start of each day
-        # if it is sunny R1 becomes 2
-        # if there is construction, then pick a random number and if it's less than .5, R1 = .1 otherwise R1 = 2 and the street crews buy all the lemonade at lunch
-
-
-
-
+        return glasses_sold
 
     end
 
